2b0d9879835d9159913793b4ccacd7dfc90ec8f0
GiakNab <giacomo.nabissi@gmail.com>
2022-05-26 13:03:39 +0200

    delete package

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/giacomo/git_workspace/omron_test_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/giacomo/git_workspace/omron_test_ws/devel;/home/giacomo/omron_test_ws/devel;/opt/ros/melodic'.split(';') if '/home/giacomo/git_workspace/omron_test_ws/devel;/home/giacomo/omron_test_ws/devel;/opt/ros/melodic' != '' else []
+underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
