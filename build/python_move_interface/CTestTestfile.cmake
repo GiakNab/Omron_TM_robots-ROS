9260152bcf1659858d0976dbddbc62b650d1cd26
GiakNab <giacomo.nabissi@gmail.com>
2022-06-02 08:44:45 +0200

    creating new pkg python_move_interface

---
+++ build/python_move_interface/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/giacomo/git_workspace/omron_test_ws/src/python_move_interface
+# Build directory: /home/giacomo/git_workspace/omron_test_ws/build/python_move_interface
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
