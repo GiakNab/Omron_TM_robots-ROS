4bba5909f445da03873db67bbbde296081b8082b
GiakNab <giacomo.nabissi@gmail.com>
2022-06-20 10:59:58 +0200

    add conveyor in gazebo world

---
+++ build/tm5_700_gripper_simulation/demo_world/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/giacomo/git_workspace/omron_test_ws/src/tm5_700_gripper_simulation/demo_world
+# Build directory: /home/giacomo/git_workspace/omron_test_ws/build/tm5_700_gripper_simulation/demo_world
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
