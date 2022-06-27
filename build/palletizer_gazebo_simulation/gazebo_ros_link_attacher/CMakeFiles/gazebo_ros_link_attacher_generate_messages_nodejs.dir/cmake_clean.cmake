f81e7b8ca5e0a28b03dbae4bf75606b4fd589235
GiakNab <giacomo.nabissi@gmail.com>
2022-06-27 15:44:41 +0200

    change name to palletizer_gazebo_simulation pkg

---
+++ build/palletizer_gazebo_simulation/gazebo_ros_link_attacher/CMakeFiles/gazebo_ros_link_attacher_generate_messages_nodejs.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gazebo_ros_link_attacher_generate_messages_nodejs"
+  "/home/giacomo/git_workspace/omron_test_ws/devel/share/gennodejs/ros/gazebo_ros_link_attacher/srv/Attach.js"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/gazebo_ros_link_attacher_generate_messages_nodejs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/palletizer_gazebo_simulation/gazebo_ros_link_attacher/CMakeFiles/gazebo_ros_link_attacher_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gazebo_ros_link_attacher_generate_messages_py"
+  "/home/giacomo/git_workspace/omron_test_ws/devel/lib/python2.7/dist-packages/gazebo_ros_link_attacher/srv/_Attach.py"
+  "/home/giacomo/git_workspace/omron_test_ws/devel/lib/python2.7/dist-packages/gazebo_ros_link_attacher/srv/__init__.py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/gazebo_ros_link_attacher_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
