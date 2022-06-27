06e0716937da20f329bba4d23c1b69ff204c2485
GiakNab <giacomo.nabissi@gmail.com>
2022-06-27 15:30:49 +0200

    rename to omronTM_python_move_interface and delete unused files

---
+++ build/omronTM_python_move_interface/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/giacomo/git_workspace/omron_test_ws/src/omronTM_python_move_interface
+# Build directory: /home/giacomo/git_workspace/omron_test_ws/build/omronTM_python_move_interface
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- devel/share/gazebo_ros_link_attacher/cmake/gazebo_ros_link_attacher-msg-extras.cmake
+++ devel/share/gazebo_ros_link_attacher/cmake/gazebo_ros_link_attacher-msg-extras.cmake
@@ -1,2 +1,2 @@
 set(gazebo_ros_link_attacher_MESSAGE_FILES "")
-set(gazebo_ros_link_attacher_SERVICE_FILES "/home/giacomo/git_workspace/omron_test_ws/src/gazebo_ros_link_attacher/srv/Attach.srv")
+set(gazebo_ros_link_attacher_SERVICE_FILES "/home/giacomo/git_workspace/omron_test_ws/src/tm5_700_gripper_simulation/gazebo_ros_link_attacher/srv/Attach.srv")
